793ffb3b2222dd396a8c08c10efb86683e1eff51
sistematawros-dev <sistematawros@gmail.com>
2026-02-17 02:01:29 -0300

    Add files via upload

---
+++ menu/src/main.js
@@ -0,0 +1,8 @@
+import { bootstrap } from './app/bootstrap.js';
+import './styles/tokens.css';
+import './styles/global.css';
+import './styles/layout.css';
+import './styles/utilities.css';
+import './styles/route-loader.css';
+
+bootstrap();
